2483b126950243c0a24630bc8d648656473260eb
phongsakon <pk@yeager.gmbh>
2024-03-25 06:07:50 +0100

    Remove unused test file and update OptimizerTests
    UnitTest1.cs, an empty test file, has been deleted. OptimizerTests.cd.cs has been substantially updated with four new test methods. These methods test functions calculating net production costs for gas and oil boilers, comparing units, and creating time series data.

--- Heatington.Tests/OptimizerTests.cd.cs
+++ Heatington.Tests/OptimizerTests.cd.cs
@@ -2,9 +2,50 @@ namespace Heatington.Tests;
 
 public class OptimizerTests
 {
+
     [Fact]
-    public void Test1()
+    public void CalculateNetProductionCost_GasBoiler_ReturnsCorrectValue()
     {
+        // Arrange
+
+        // Act
+
+        // Assert
 
     }
+
+    [Fact]
+    public void CalculateNetProductionCost_OilBoiler_ReturnsCorrectValue()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public void CompareUnits_GasAndOilBoiler_ReturnsCorrectValue()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+
+    [Fact]
+    public void CreateTimeSeriesData_OptimizedData_ReturnsSuccessBool()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+
+
+
 }
