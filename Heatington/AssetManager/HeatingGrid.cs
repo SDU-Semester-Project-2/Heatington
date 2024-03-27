b45e71690c6290ab55d765d3c641e382918797bc
KonyakB <bencekanyok@gmail.com>
2024-03-27 10:50:45 +0100

    SP2-84 - Commented on a field to make it more clear

--- Heatington/AssetManager/HeatingGrid.cs
+++ Heatington/AssetManager/HeatingGrid.cs
@@ -3,7 +3,7 @@ namespace Heatington.AssetManager;
 public class HeatingGrid
 {
     public Guid Id { get; set; }
-    public string Picture { get; set; }
+    public string Picture { get; set; } //path to picture of the grid
     public string Name { get; set; }
 
     public HeatingGrid(string picture, string name)
