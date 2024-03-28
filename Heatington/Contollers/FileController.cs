acf178f9eb6eefe2ccdc822a85c11a2e0a440013
magwrap <jasiek.musiol@gmail.com>
2024-03-28 22:58:22 +0100

    SP2-85 - added documentation(comments) to FileController

---
+++ Heatington/Contollers/FileController.cs
@@ -0,0 +1,112 @@
+namespace Heatington.Contollers;
+
+/// <summary>
+/// Class for performing read/write actions on local files
+/// </summary>
+public class FileController : IReadWriteController
+{
+    /// <summary>
+    /// Path to a file
+    /// </summary>
+    private readonly string _path;
+
+
+    /// <summary>
+    /// Class constructor. Gets a path to the file to control.
+    /// </summary>
+    /// <param name="pathToFile"> Path to the location of a file.
+    /// Follows the pattern <c>file.json</c>
+    /// </param>
+    public FileController(string pathToFile)
+    {
+        _path = pathToFile;
+    }
+
+    /// <summary>
+    /// Simple eye-candy for better readability of errors
+    /// </summary>
+    /// <param name="message">Error message to display</param>
+    private static void DisplayException(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    /// <summary>
+    /// Helper method performing try-catch clauses in file oriented manner
+    /// </summary>
+    /// <param name="funcToTry">Function to run inside of try-catch block</param>
+    /// <typeparam name="T">Return type of a function</typeparam>
+    /// <returns>Return the outcome of run function, if function return void, make the lambda function to return 0</returns>
+    private T TryFileOperationRunner<T>(Func<T> funcToTry)
+    {
+        try
+        {
+            return funcToTry();
+        }
+        catch (FileNotFoundException e)
+        {
+            DisplayException($"File does not exist!\nPath: {_path}\n{e.FileName}");
+            throw;
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            DisplayException($"You passed a wrong path! Directory does not exist.\nPath: {_path}\n{e.Message}");
+            throw;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unknown exception: {e}");
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Function for reading the contents of a local file.
+    /// Reads from the path passed during object initialization.
+    /// </summary>
+    /// <returns>Returns content of the file as a string</returns>
+    /// <exception cref="FileNotFoundException">If file does not exist or path is not correct the exception is thrown</exception>
+    private string? ReadFileFromPath()
+    {
+        string? fileContent = TryFileOperationRunner(() =>
+        {
+            return File.ReadAllText(_path);
+        });
+
+        return fileContent;
+    }
+
+    /// <summary>
+    /// Function for writing string data into a local file from a path
+    /// </summary>
+    /// <param name="content">Content to be written to a file as a string</param>
+    private void WriteToFileFromPath(string content)
+    {
+        TryFileOperationRunner(() =>
+        {
+            File.WriteAllText(_path, content);
+            return 0;
+        });
+    }
+
+    /// <summary>
+    /// Function for reading the data of a local file.
+    /// Reads from the path passed during object initialization.
+    /// </summary>
+    /// <returns>Returns data of the file as a string</returns>
+    public string? ReadData()
+    {
+        return ReadFileFromPath();
+    }
+
+    /// <summary>
+    /// Function for writing data into a local file
+    /// </summary>
+    /// <param name="content">Content to be written to a file as a string</param>
+    public void WriteData(string content)
+    {
+        WriteToFileFromPath(content);
+    }
+}
