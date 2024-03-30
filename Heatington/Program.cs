022d4bb70ab81e463fbfc121eeba269015c98550
phongsakon <pk@yeager.gmbh>
2024-03-30 10:09:28 +0100

    SP2-90 - Add temporary main method in Program.cs
    A main method has been temporarily added in the Program.cs file for testing purposes. This method is not the final implementation and will be replaced when the actual application logic is implemented.

--- Heatington/Program.cs
+++ Heatington/Program.cs
@@ -2,6 +2,8 @@ namespace Heatington
 {
     class Program
     {
+        // This is only for the purpose of testing the implementation
+        // TODO: Remove this method and implement the actual application logic
         static void Main(string[] args)
         {
         }
