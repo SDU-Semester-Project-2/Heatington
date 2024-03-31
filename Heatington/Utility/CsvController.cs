35ad149a087d21bee1d58b0f988ef37f78dc8766
0rangeBlaze <gardevdaniel@gmail.com>
2024-03-31 12:53:51 +0200

    SP2-91 - CSV serialization formats correctly

--- Heatington/Utility/CsvController.cs
+++ Heatington/Utility/CsvController.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 namespace Heatington.Utility
 {
-    [System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple=false)]
+    [System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple = false)]
     public sealed class CsvConstructorAttribute : Attribute;
 
     class CsvData
@@ -13,14 +13,16 @@ namespace Heatington.Utility
         public CsvData(List<string[]> data, string[] header = null)
         {
             int numberOfFields = data[0].Length;
-            if(!data.TrueForAll(x => x.Length == numberOfFields))
+            if (!data.TrueForAll(x => x.Length == numberOfFields))
             {
                 throw new Exception("Number of fields not consistent throughout csv.");
             }
-            if(header != null && header.Length != numberOfFields)
+
+            if (header != null && header.Length != numberOfFields)
             {
                 throw new Exception("Number of fields in csv header does not match number of fields in csv body.");
             }
+
             Table = data;
             Header = header;
         }
@@ -30,7 +32,8 @@ namespace Heatington.Utility
             PropertyInfo[] props = typeof(T).GetProperties();
             List<string[]> res = data.Select(
                 x => props.Select(
-                    prop => {
+                    prop =>
+                    {
                         object? value = prop.GetValue(x);
                         return value != null ? value.ToString() : "";
                     }
@@ -44,34 +47,40 @@ namespace Heatington.Utility
             ConstructorInfo ctor;
             ConstructorInfo[] allCtors = typeof(T).GetConstructors();
             Console.WriteLine(allCtors.Length);
-            if(allCtors.Length == 1)
+            if (allCtors.Length == 1)
             {
                 ctor = allCtors[0];
             }
             else
             {
-                ConstructorInfo[] csvConstructors = allCtors.Where(x => Attribute.IsDefined(x, typeof(CsvConstructorAttribute))).ToArray();
-                if(csvConstructors.Length == 0)
+                ConstructorInfo[] csvConstructors =
+                    allCtors.Where(x => Attribute.IsDefined(x, typeof(CsvConstructorAttribute))).ToArray();
+                if (csvConstructors.Length == 0)
                 {
-                    throw new Exception($"The type '{typeof(T)}' must have at one constructor with CsvConsturcotrAttribute.");
+                    throw new Exception(
+                        $"The type '{typeof(T)}' must have at one constructor with CsvConsturcotrAttribute.");
                 }
-                else if(csvConstructors.Length > 1)
+                else if (csvConstructors.Length > 1)
                 {
-                    throw new Exception($"The type '{typeof(T)}' must have at most one constructor with CsvConsturcotrAttribute.");
+                    throw new Exception(
+                        $"The type '{typeof(T)}' must have at most one constructor with CsvConsturcotrAttribute.");
                 }
                 else
                 {
                     ctor = csvConstructors[0];
                 }
             }
+
             ParameterInfo[] parameters = ctor.GetParameters();
             Dictionary<string, ParameterInfo> paramDict = parameters.ToDictionary(param => param.Name, param => param);
 
             List<T> res = new();
             if (parameters.Length != Table[0].Length)
             {
-                throw new Exception($"Number of parameters in {typeof(T)}'s constructor does not match number of entries in a record of the CsvTable.");
+                throw new Exception(
+                    $"Number of parameters in {typeof(T)}'s constructor does not match number of entries in a record of the CsvTable.");
             }
+
             bool useHeader = Header != null && checkMatchesParameters(paramDict);
             foreach (string[] values in Table)
             {
@@ -82,14 +91,17 @@ namespace Heatington.Utility
                     var value = Convert.ChangeType(values[i], currentParam.ParameterType);
                     parameterValues[currentParam.Position] = value;
                 }
-                res.Add((T) ctor.Invoke(parameterValues));
+
+                res.Add((T)ctor.Invoke(parameterValues));
             }
+
             return res;
         }
 
         private bool checkMatchesParameters(Dictionary<string, ParameterInfo> paramDict)
         {
-            return (Header.Distinct().Count() == Header.Length) && Array.TrueForAll(Header, x => paramDict.ContainsKey(x));
+            return (Header.Distinct().Count() == Header.Length) &&
+                   Array.TrueForAll(Header, x => paramDict.ContainsKey(x));
         }
     }
 
@@ -102,15 +114,15 @@ namespace Heatington.Utility
             List<string[]> all = new();
             bool isDoubleQuoted = false;
             string current = "";
-            while(i<rawData.Length)
+            while (i < rawData.Length)
             {
                 isDoubleQuoted = false;
                 current = "";
-                if(rawData[i] == '"')
+                if (rawData[i] == '"')
                 {
                     isDoubleQuoted = true;
                 }
-                else if(rawData[i] == '\n')
+                else if (rawData[i] == '\n')
                 {
                     throw new Exception("Entries can't start with new line.");
                 }
@@ -121,25 +133,25 @@ namespace Heatington.Utility
 
                 i++;
                 bool inEntry = true;
-                while(inEntry && i < rawData.Length)
+                while (inEntry && i < rawData.Length)
                 {
-                    if(rawData[i] == '"')
+                    if (rawData[i] == '"')
                     {
-                        if(isDoubleQuoted)
+                        if (isDoubleQuoted)
                         {
-                            if(i+1 < rawData.Length)
+                            if (i + 1 < rawData.Length)
                             {
                                 i++;
-                                if(rawData[i] == '"')
+                                if (rawData[i] == '"')
                                 {
                                     current += rawData[i];
                                 }
-                                else if(rawData[i] == ',')
+                                else if (rawData[i] == ',')
                                 {
                                     currentRecord.Add(current);
                                     inEntry = false;
                                 }
-                                else if(rawData[i] == '\n')
+                                else if (rawData[i] == '\n')
                                 {
                                     currentRecord.Add(current);
                                     all.Add(currentRecord.ToArray());
@@ -161,24 +173,25 @@ namespace Heatington.Utility
                         }
                         else
                         {
-                            throw new Exception("To have a '\"' character in a CSV entry you need to encoles the entry with '\"' and escape the required '\"' with another '\"'.");
+                            throw new Exception(
+                                "To have a '\"' character in a CSV entry you need to encoles the entry with '\"' and escape the required '\"' with another '\"'.");
                         }
                     }
-                    else if(rawData[i] == ',')
+                    else if (rawData[i] == ',')
                     {
-                        if(isDoubleQuoted)
+                        if (isDoubleQuoted)
                         {
                             current += rawData[i];
                         }
-                        else 
+                        else
                         {
                             currentRecord.Add(current);
                             inEntry = false;
                         }
                     }
-                    else if(rawData[i] == '\n')
+                    else if (rawData[i] == '\n')
                     {
-                        if(isDoubleQuoted)
+                        if (isDoubleQuoted)
                         {
                             current += rawData[i];
                         }
@@ -194,11 +207,13 @@ namespace Heatington.Utility
                     {
                         current += rawData[i];
                     }
+
                     i++;
                 }
-                if(inEntry)
+
+                if (inEntry)
                 {
-                    if(isDoubleQuoted)
+                    if (isDoubleQuoted)
                     {
                         throw new Exception("All entries have to close the opened '\"'.");
                     }
@@ -211,17 +226,28 @@ namespace Heatington.Utility
                     }
                 }
             }
+
             return new CsvData(
                 includesHeader ? all[1..] : all,
                 includesHeader ? all[0] : null
             );
-            
         }
 
         public static string Serialize(CsvData data)
         {
-            return (data.Header != null ? String.Join(", ", data.Header) : "") + '\n' +
-                    String.Join("\n", data.Table.Select(x => String.Join(", ", x)));
+            char[] requiresQuotes = new[] { ',', '\n', '"' };
+            List<string[]> escapedTable = data.Table.Select(x =>
+                x.Select(str =>
+                    requiresQuotes.Any(chr => str.Contains(chr))
+                        ? '"' + String.Join("\"\"", str.Split('"')) + '"'
+                        : str).ToArray()).ToList();
+            string[] escapedHeader = data.Header.Select(str =>
+                    requiresQuotes.Any(chr => str.Contains(chr))
+                        ? '"' + String.Join("\"\"", str.Split('"')) + '"'
+                        : str)
+                .ToArray();
+            return (escapedHeader != null ? String.Join(",", escapedHeader) : "") + '\n' +
+                   String.Join("\n", escapedTable.Select(x => String.Join(",", x)));
         }
     }
 }
