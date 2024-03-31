53f8d60807c3da0f857ec2eaec537efc3fa71a11
phongsakon <pk@yeager.gmbh>
2024-03-31 06:51:23 +0200

    SP2-90 Update documentation and add TODOs in IDataSource and CsvDataSource
    Added summary and remarks in the comments section for both IDataSource and CsvDataSource classes. The remarks contain information about the classes' eventual replacement with CsvController.cs and the need to delete them during the refactoring phase.

--- Heatington/Data/IDataSource.cs
+++ Heatington/Data/IDataSource.cs
@@ -2,9 +2,27 @@ using Heatington.Models;
 
 namespace Heatington.Data
 {
+    /// <summary>
+    /// Represents a data source for reading and writing data.
+    /// <remarks>
+    /// This is replaced by CsvController.cs, the current existence is only for the purpose of the documentation
+    /// </remarks>
+    /// TODO: Delete this interface in the refactoring phase
+    /// </summary>
     public interface IDataSource
     {
+        /// <summary>
+        /// Retrieves the time series data from the specified data source.
+        /// </summary>
+        /// <param name="filePath">The file path of the CSV file containing the data.</param>
+        /// <returns>A list of DataPoint objects representing the heat demand and electricity price data.</returns>
         List<DataPoint>? GetData(string filePath);
+
+        /// <summary>
+        /// Saves the given data points to a CSV file at the specified file path.
+        /// </summary>
+        /// <param name="data">The list of data points to be saved.</param>
+        /// <param name="filePath">The file path where the CSV file will be saved.</param>
         void SaveData(List<DataPoint> data, string filePath);
     }
 }
