ab4bfb95e320d39d86c5bf8210490be83fae93fd
0rangeBlaze <gardevdaniel@gmail.com>
2024-04-01 11:01:28 +0200

    SP2-91 - Formatting

--- Heatington/Utility/CsvController.cs
+++ Heatington/Utility/CsvController.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 namespace CsvHandle
 {
-    [System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple=false)]
+    [System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple = false)]
     public sealed class CsvConstructorAttribute : Attribute;
 
     class CsvData
@@ -13,11 +13,11 @@ namespace CsvHandle
         public CsvData(List<string[]> data, string[]? header = null)
         {
             int numberOfFields = data[0].Length;
-            if(!data.TrueForAll(x => x.Length == numberOfFields))
+            if (!data.TrueForAll(x => x.Length == numberOfFields))
             {
                 throw new Exception("Number of fields not consistent throughout csv.");
             }
-            if(header != null && header.Length != numberOfFields)
+            if (header != null && header.Length != numberOfFields)
             {
                 throw new Exception("Number of fields in csv header does not match number of fields in csv body.");
             }
@@ -31,7 +31,8 @@ namespace CsvHandle
             header = header ?? props.Select(x => x.Name).ToArray();
             List<string[]> res = data.Select(
                 x => props.Select(
-                    prop => {
+                    prop =>
+                    {
                         object? value = prop.GetValue(x);
                         return value != null ? (value.ToString() ?? "") : "";
                     }
@@ -45,18 +46,18 @@ namespace CsvHandle
             ConstructorInfo ctor;
             ConstructorInfo[] allCtors = typeof(T).GetConstructors();
             Console.WriteLine(allCtors.Length);
-            if(allCtors.Length == 1)
+            if (allCtors.Length == 1)
             {
                 ctor = allCtors[0];
             }
             else
             {
                 ConstructorInfo[] csvConstructors = allCtors.Where(x => Attribute.IsDefined(x, typeof(CsvConstructorAttribute))).ToArray();
-                if(csvConstructors.Length == 0)
+                if (csvConstructors.Length == 0)
                 {
                     throw new Exception($"The type '{typeof(T)}' must have at one constructor with CsvConsturcotrAttribute.");
                 }
-                else if(csvConstructors.Length > 1)
+                else if (csvConstructors.Length > 1)
                 {
                     throw new Exception($"The type '{typeof(T)}' must have at most one constructor with CsvConsturcotrAttribute.");
                 }
@@ -83,7 +84,7 @@ namespace CsvHandle
                     var value = Convert.ChangeType(values[i], currentParam.ParameterType);
                     parameterValues[currentParam.Position] = value;
                 }
-                res.Add((T) ctor.Invoke(parameterValues));
+                res.Add((T)ctor.Invoke(parameterValues));
             }
             return res;
         }
@@ -103,15 +104,15 @@ namespace CsvHandle
             List<string[]> all = new();
             bool isDoubleQuoted = false;
             string current = "";
-            while(i<rawData.Length)
+            while (i < rawData.Length)
             {
                 isDoubleQuoted = false;
                 current = "";
-                if(rawData[i] == '"')
+                if (rawData[i] == '"')
                 {
                     isDoubleQuoted = true;
                 }
-                else if(rawData[i] == '\n')
+                else if (rawData[i] == '\n')
                 {
                     throw new Exception("Entries can't start with new line.");
                 }
@@ -122,25 +123,25 @@ namespace CsvHandle
 
                 i++;
                 bool inEntry = true;
-                while(inEntry && i < rawData.Length)
+                while (inEntry && i < rawData.Length)
                 {
-                    if(rawData[i] == '"')
+                    if (rawData[i] == '"')
                     {
-                        if(isDoubleQuoted)
+                        if (isDoubleQuoted)
                         {
-                            if(i+1 < rawData.Length)
+                            if (i + 1 < rawData.Length)
                             {
                                 i++;
-                                if(rawData[i] == '"')
+                                if (rawData[i] == '"')
                                 {
                                     current += rawData[i];
                                 }
-                                else if(rawData[i] == ',')
+                                else if (rawData[i] == ',')
                                 {
                                     currentRecord.Add(current);
                                     inEntry = false;
                                 }
-                                else if(rawData[i] == '\n')
+                                else if (rawData[i] == '\n')
                                 {
                                     currentRecord.Add(current);
                                     all.Add(currentRecord.ToArray());
@@ -165,21 +166,21 @@ namespace CsvHandle
                             throw new Exception("To have a '\"' character in a CSV entry you need to encoles the entry with '\"' and escape the required '\"' with another '\"'.");
                         }
                     }
-                    else if(rawData[i] == ',')
+                    else if (rawData[i] == ',')
                     {
-                        if(isDoubleQuoted)
+                        if (isDoubleQuoted)
                         {
                             current += rawData[i];
                         }
-                        else 
+                        else
                         {
                             currentRecord.Add(current);
                             inEntry = false;
                         }
                     }
-                    else if(rawData[i] == '\n')
+                    else if (rawData[i] == '\n')
                     {
-                        if(isDoubleQuoted)
+                        if (isDoubleQuoted)
                         {
                             current += rawData[i];
                         }
@@ -197,9 +198,9 @@ namespace CsvHandle
                     }
                     i++;
                 }
-                if(inEntry)
+                if (inEntry)
                 {
-                    if(isDoubleQuoted)
+                    if (isDoubleQuoted)
                     {
                         throw new Exception("All entries have to close the opened '\"'.");
                     }
@@ -216,15 +217,36 @@ namespace CsvHandle
                 includesHeader ? all[1..] : all,
                 includesHeader ? all[0] : null
             );
-            
+
         }
 
         public static string Serialize(CsvData data, bool includeHeaderIfNotNull = true)
         {
-            char[] requiresQuotes = new[] {',', '\n', '"'};
-            List<string[]> escapedTable = data.Table.Select(x => x.Select(str => requiresQuotes.Any(chr => str.Contains(chr)) ? '"' + String.Join("\"\"", str.Split('"')) + '"' : str).ToArray()).ToList();
-            string[]? escapedHeader = data.Header != null ? data.Header.Select(str => requiresQuotes.Any(chr => str.Contains(chr)) ? '"' + String.Join("\"\"", str.Split('"')) + '"' : str).ToArray() : null;
-            return ((escapedHeader != null && includeHeaderIfNotNull) ? String.Join(",", escapedHeader) : "") + '\n' + 
+            char[] requiresQuotes = new[] { ',', '\n', '"' };
+            List<string[]> escapedTable =
+                data.Table.Select(
+                    arr => arr.Select(
+                        str =>
+                        requiresQuotes.Any(chr => str.Contains(chr))
+                        ?
+                        '"' + String.Join("\"\"", str.Split('"')) + '"'
+                        :
+                        str
+                    ).ToArray()
+                ).ToList();
+
+            string[]? escapedHeader = data.Header != null ?
+                data.Header.Select(
+                    str =>
+                    requiresQuotes.Any(chr => str.Contains(chr))
+                    ?
+                    '"' + String.Join("\"\"", str.Split('"')) + '"'
+                    :
+                    str
+                ).ToArray()
+                : null;
+
+            return ((escapedHeader != null && includeHeaderIfNotNull) ? String.Join(",", escapedHeader) : "") + '\n' +
                     String.Join("\n", escapedTable.Select(x => String.Join(",", x)));
         }
     }
