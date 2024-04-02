027ce7fdb1f977ce9b6c44748c5279af37c049a8
phongsakon <pk@yeager.gmbh>
2024-04-02 13:48:32 +0200

    SP2-90-Remove excessive comments from DataPoint and SourceDataManager
    The comments that were explaining obvious code behavior were removed. The code is now more clean and readable. This way, developers can quickly understand the code without sifting through unnecessary comments.

--- Heatington/Models/DataPoint.cs
+++ Heatington/Models/DataPoint.cs
@@ -3,14 +3,10 @@ using Heatington.Utility;
 
 namespace Heatington.Models
 {
-    /// <summary>
-    /// Represents a data point containing information about heat demand and electricity price.
-    /// </summary>
+
     public class DataPoint
     {
-        /// <summary>
-        /// Represents a data point containing information about heat demand and electricity price.
-        /// </summary>
+
         [CsvConstructor]
         public DataPoint(string startTime, string endTime, string heatDemand, string electricityPrice)
         {
@@ -22,24 +18,12 @@ namespace Heatington.Models
 
         // TODO: Maybe implement factory method to increase the abstraction
 
-        /// <summary>
-        /// Gets the start time of a data point.
-        /// </summary>
         public DateTime StartTime { get; }
 
-        /// <summary>
-        /// Gets the end time of the data point.
-        /// </summary>
         public DateTime EndTime { get; }
 
-        /// <summary>
-        /// Represents a data point with heat demand information.
-        /// </summary>
         public double HeatDemand { get; }
 
-        /// <summary>
-        /// Represents the electricity price for a given data point.
-        /// </summary>
         public double ElectricityPrice { get; }
     }
 }
