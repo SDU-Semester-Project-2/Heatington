e9ed4028622173ea48e0fc8084ac14220b4f04ea
phongsakon <pk@yeager.gmbh>
2024-04-02 06:50:14 +0200

    SP2-90-Refactor data handling and fix data formatting
    The Main method in Program.cs has been cleaned up, removing old implementation logic and clearing embedded comments. 'FilePath' has been replaced with '_filePath' in SourceDataManager.cs for data handling consistency. TimeSeriesData is now public.
    
    Data formatting of 'HeatDemand' and 'ElectricityPrice' in the DataPoint.cs model has been fixed - they were previously multiplied by 100 due to a formatting issue.

--- Heatington/Program.cs
+++ Heatington/Program.cs
@@ -4,24 +4,25 @@ namespace Heatington
 {
     internal static class Program
     {
-        // This is only for the purpose of testing the implementation
-        // TODO: Rewrite this method and implement the actual application logic
+
         static void Main(string[] args)
         {
-            const string filePath = "../Assets/winter_period.csv";
 
-            // Create a new CsvDataSource
-            IDataSource dataSource = new CsvDataSource();
-
-            SourceDataManager sdm = new(dataSource, filePath);
-
-            // Fetch data from dataSource
-            sdm.FetchTimeSeriesData();
-
-            // Log the loaded data to the console
-            sdm.LogTimeSeriesData();
-
-            Console.WriteLine("Data loaded successfully.");
         }
     }
 }
+        //     // SDM Test
+        //     const string filePath = "../Assets/winter_period.csv";
+        //
+        //     // Create a new CsvDataSource
+        //     IDataSource dataSource = new CsvDataSource();
+        //
+        //     SourceDataManager sdm = new(dataSource, filePath);
+        //
+        //     // Fetch data from dataSource
+        //     sdm.FetchTimeSeriesData();
+        //
+        //     // Log the loaded data to the console
+        //     sdm.LogTimeSeriesData();
+        //
+        //     Console.WriteLine("Data loaded successfully.");
