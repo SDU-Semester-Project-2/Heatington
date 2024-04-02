254075968a898345e43d680679f984eecedd9cd3
phongsakon <pk@yeager.gmbh>
2024-04-02 13:32:51 +0200

    SP2-90-Add detailed documentation for data access classes and removed documentation in the codebase.
    Replaced the documentation for the `CsvDataSource`, `SourceDataManager`, `IDataSource`, and `DataPoint` classes. These markdown files contain information such as overview, methods, properties, and ideas for future implementation.

--- Heatington/Data/IDataSource.cs
+++ Heatington/Data/IDataSource.cs
@@ -2,23 +2,11 @@ using Heatington.Models;
 
 namespace Heatington.Data
 {
-    /// <summary>
-    /// Represents a data source for reading and writing data.
-    /// </summary>
+
     public interface IDataSource
     {
-        /// <summary>
-        /// Retrieves the time series data from the specified data source.
-        /// </summary>
-        /// <param name="filePath">The file path of the CSV file containing the data.</param>
-        /// <returns>A list of DataPoint objects representing the heat demand and electricity price data.</returns>
-        List<DataPoint>? GetData(string filePath);
 
-        /// <summary>
-        /// Saves the given data points to a CSV file at the specified file path.
-        /// </summary>
-        /// <param name="data">The list of data points to be saved.</param>
-        /// <param name="filePath">The file path where the CSV file will be saved.</param>
+        List<DataPoint>? GetData(string filePath);
         void SaveData(List<DataPoint> data, string filePath);
     }
 }
--- Heatington/Program.cs
+++ Heatington/Program.cs
@@ -1,4 +1,4 @@
-﻿using Heatington.Data;
+﻿
 
 namespace Heatington
 {
@@ -11,18 +11,3 @@ namespace Heatington
         }
     }
 }
-        //     // SDM Test
-        //     const string filePath = "../Assets/winter_period.csv";
-        //
-        //     // Create a new CsvDataSource
-        //     IDataSource dataSource = new CsvDataSource();
-        //
-        //     SourceDataManager sdm = new(dataSource, filePath);
-        //
-        //     // Fetch data from dataSource
-        //     sdm.FetchTimeSeriesData();
-        //
-        //     // Log the loaded data to the console
-        //     sdm.LogTimeSeriesData();
-        //
-        //     Console.WriteLine("Data loaded successfully.");
