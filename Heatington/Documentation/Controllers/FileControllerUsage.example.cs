77994fac6b52b4f72ae80aa8ad3ebc505d0439ef
magwrap <jasiek.musiol@gmail.com>
2024-04-02 12:46:54 +0200

    SP2-85 - moved DisplayErrorMessage to Utilities class, documentation changes, fix spelling error

--- Heatington/Documentation/Controllers/FileControllerUsage.example.cs
+++ Heatington/Documentation/Controllers/FileControllerUsage.example.cs
@@ -5,6 +5,30 @@ using Heatington.Contollers;
 
 namespace Heatington.Documentation.Controllers;
 
+///<example>
+/// <code>
+/// string DefaultAssetsDirectory = "Assets/";
+///
+/// string? pathToParentDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.FullName;
+/// string? path;
+/// string fileName = "testFile.json";
+///
+/// if (pathToParentDirectory != null)
+/// {
+/// path = Path.Combine(pathToParentDirectory, DefaultAssetsDirectory + fileName);
+/// }
+/// else
+/// {
+/// throw new DirectoryNotFoundException($"The directory {pathToParentDirectory} does not exist.\n" +
+/// $"This is an error caused by FileController, something went wrong when getting the parent directory");
+/// }
+///
+/// IReadWriteController fileController = new FileController(path);
+/// string? data = fileController.ReadData();
+///
+/// Console.WriteLine(data);
+///  </code>
+/// </example>
 unsafe class FileControllerExampleUsage
 {
     FileControllerExampleUsage()
