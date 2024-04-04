c249e5c89e014ac0e76cddc63608f1dcbe00fc91
frske <144455292+frske@users.noreply.github.com>
2024-04-04 15:56:18 +0200

    Created ResultHolder model
    minor refactor once AM is here

---
+++ Heatington/Models/ResultHolder.cs
@@ -0,0 +1,36 @@
+using Heatington.Optimizer;
+
+namespace Heatington.Models;
+
+public class ResultHolder
+{
+    public DateTime StartTime { get; }
+    public DateTime EndTime { get; }
+    public double HeatDemand { get; }
+    public double ElectricityPrice { get; }
+    public List<ProductionUnit> Boilers { get; set; }
+
+    public override string ToString()
+    {
+        TimeZoneInfo danishTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+        DateTime startTimeInDanish = TimeZoneInfo.ConvertTimeFromUtc(StartTime, danishTimeZone);
+        DateTime endTimeInDanish = TimeZoneInfo.ConvertTimeFromUtc(EndTime, danishTimeZone);
+
+        string formattedStart = startTimeInDanish.ToString("dd.MM.yyyy HH:mm");
+        string formattedEnd = endTimeInDanish.ToString("dd.MM.yyyy HH:mm");
+
+        string boilers = string.Empty;
+
+        foreach (ProductionUnit productionUnit in Boilers)
+        {
+            boilers = string.Concat(boilers, "\n", $"Name: {productionUnit.Name} ",
+                $"Production cost: {productionUnit.ProductionCost }",
+                $"Operation point: {productionUnit.OperationPoint}");
+        }
+
+        string s = string.Concat($"Start Time: {formattedStart} ", $"End Time: {formattedEnd}; ",
+            $"Heat Demand: {HeatDemand} MWh; ", $"Electricity Price: {ElectricityPrice} DKK/MWh", boilers);
+
+        return s;
+    }
+}
