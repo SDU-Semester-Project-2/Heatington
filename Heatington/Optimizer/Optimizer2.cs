80accc01afa62977d8ce4c37b23f0066d55762cd
frske <144455292+frske@users.noreply.github.com>
2024-04-04 13:33:56 +0200

    SP2-98 - Get Production units + helper method to log loaded units
    Will need to be implemented again once AM is here

--- Heatington/Optimizer/Optimizer2.cs
+++ Heatington/Optimizer/Optimizer2.cs
@@ -7,22 +7,10 @@ namespace Heatington.Optimizer;
 public class Optimizer2
 {
     private List<DataPoint>? _dataPoints = new List<DataPoint>();
+    private List<ProductionUnit> _productionUnits = new List<ProductionUnit>();
 
 
 
-
-    public void LogDataPoints()
-    {
-        if (_dataPoints == null)
-        {
-            return;
-        }
-
-        foreach (DataPoint dataPoint in _dataPoints)
-        {
-            Console.WriteLine(dataPoint);
-        }
-    }
 
     public void GetDataPoints()
     {
@@ -37,5 +25,38 @@ public class Optimizer2
         //sourceDataManager.LogTimeSeriesData();
 
         _dataPoints = sourceDataManager.TimeSeriesData;
+    }
+
+    public void LogDataPoints()
+    {
+        if (_dataPoints == null)
+        {
+            return;
+        }
+
+        foreach (DataPoint dataPoint in _dataPoints)
+        {
+            Console.WriteLine(dataPoint);
+        }
+    }
+
+    // Will call Asset Manager eventually.
+    public void GetProductionUnits()
+    {
+        ProductionUnit gasBoiler = new ProductionUnit("Gas Boiler", "", 5,500,0,1.1,215);
+        ProductionUnit oilBoiler = new ProductionUnit("Oil Boiler", "", 4,700,0,1.2,265);
+        ProductionUnit controlBoiler = new ProductionUnit("Control Boiler", "", 5,800,0,1.5,310);
+
+        _productionUnits.Add(gasBoiler);
+        _productionUnits.Add(oilBoiler);
+        _productionUnits.Add(controlBoiler);
+    }
+
+    public void LogProductionUnits()
+    {
+        foreach (ProductionUnit productionUnit in _productionUnits)
+        {
+            Console.WriteLine(productionUnit);
+        }
     }
 }
