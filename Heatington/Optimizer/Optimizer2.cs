944563e815e10c3b98ae3f6d0c114253dfbfa898
frske <144455292+frske@users.noreply.github.com>
2024-04-04 13:03:56 +0200

    SP2-98 - Implemented GetDataPoints with async

--- Heatington/Optimizer/Optimizer2.cs
+++ Heatington/Optimizer/Optimizer2.cs
@@ -1,9 +1,29 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+using Heatington.Data;
+using Heatington.Models;
+using Heatington.SourceDataManager;
 
 namespace Heatington.Optimizer;
 
 public class Optimizer2
 {
-    
+    private List<DataPoint>? _dataPoints = new List<DataPoint>();
+
+
+
+
+
+    public void GetDataPoints()
+    {
+        IDataSource dataSource = new CsvDataSource();
+
+        SourceDataManager.SourceDataManager sourceDataManager = new(dataSource, "../../../../Assets/winter_period.csv");
+
+        Task fetchTimeSeriesDataAsync = sourceDataManager.FetchTimeSeriesDataAsync();
+
+        fetchTimeSeriesDataAsync.Wait();
+
+        //sourceDataManager.LogTimeSeriesData();
+
+        _dataPoints = sourceDataManager.TimeSeriesData;
+    }
 }
