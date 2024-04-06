84c2f59b26a45059864b52899f0a53af6bb04c24
Rikki Snaebjornsson <R.Snaebjornsson@gmail.com>
2024-04-06 19:48:52 +0200

    SP2-14 - Added ResultDataManager class and initialized it's documentation.

---
+++ Heatington/ResultDataManager/ResultDataManager.cs
@@ -0,0 +1,20 @@
+using Heatington.Contollers;
+
+namespace Heatington.ResultDataManager
+{
+
+    public class ResultDataManager
+    {
+        private readonly string _filePath;
+
+        public ResultDataManager(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void WriteOptimizationResultToCSV()
+        {
+
+        }
+    }
+}
