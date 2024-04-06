a9b9c362b1a09a0fddae43a3581da710aa669c05
magwrap <jasiek.musiol@gmail.com>
2024-04-06 23:12:18 +0200

    SP2-86 - added core assests and properties for AssetManager

---
+++ Heatington/AssetManager/AssetManager.cs
@@ -0,0 +1,31 @@
+namespace Heatington.AssetManager;
+
+public class AssetManager
+{
+    public HeatingGrid HeatingGridInformation;
+    public Dictionary<string, ProductionUnit> ProductionUnits;
+
+
+    public ProductionUnit ReadHeatingUnits()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WriteHeatingUnit()
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
+
+// Asset Manager
+//
+// + HeatingGridInformation: HeatingGrid
+//
+// + ProductionUnits: Dictionary<ProductionUnit>
+//
+// + ReadHeatingUnits(): ProductionUnit
+//
+// + WriteHeatingUnit
+
