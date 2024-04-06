bfd7ababa86b5b2b8555ec972880b4166c2eff27
Rikki Snaebjornsson <R.Snaebjornsson@gmail.com>
2024-04-06 23:24:56 +0200

    SP2-14 - Added ConvertOptResultsToData() method

--- Heatington/ResultDataManager/ResultDataManager.cs
+++ Heatington/ResultDataManager/ResultDataManager.cs
@@ -4,17 +4,15 @@ using Heatington.Optimizer;
 
 namespace Heatington.ResultDataManager
 {
-
     public class ResultDataManager
     {
         private readonly string _filePath;
-        private readonly List<string> _data = new List<string>();
+        private readonly List<string[]> _data = new List<string[]>();
         private List<ResultHolder> _optResults;
 
         public ResultDataManager(string filePath)
         {
             _filePath = filePath;
-
         }
 
         public void FetchOptimizationData(Opt opt)
@@ -22,11 +20,32 @@ namespace Heatington.ResultDataManager
             _optResults = opt.Results;
         }
 
+        public void ConvertOptResultsToData()
+        {
+            foreach (ResultHolder result in _optResults)
+            {
+                List<string> list = new List<string>();
+                list.Append(result.StartTime.ToString());
+                list.Append(result.EndTime.ToString());
+                list.Append(result.HeatDemand.ToString());
+                list.Append(result.ElectricityPrice.ToString());
+                list.Append(result.NetProductionCost.ToString());
+                foreach (ProductionUnit boiler in result.Boilers)
+                {
+                    list.Append(boiler.ToString());
+                }
+
+                _data.Append(list.ToArray());
+            }
+
+            //Console.WriteLine(_data.Count());
+        }
+
         public void WriteDataToCsv()
         {
             CsvData csvData = new CsvData(_data);
 
-            String serializedString = CsvController.Serialize(_csvData);
+            String serializedString = CsvController.Serialize(csvData);
 
 
         }
