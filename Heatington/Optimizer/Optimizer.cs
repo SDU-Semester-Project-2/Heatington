339fb6420121507594cc4cbd2cb869e179f1288d
frske <144455292+frske@users.noreply.github.com>
2024-04-06 21:16:17 +0200

    SP2-98 - set correct access modifiers for properties

--- Heatington/Optimizer/Optimizer.cs
+++ Heatington/Optimizer/Optimizer.cs
@@ -1,123 +1,210 @@
-using FrancescoDummyClasses;
+using Heatington.Data;
+using Heatington.Models;
 
 namespace Heatington.Optimizer;
 
 public class Opt
 {
-    private List<ProductionUnitFs> _pUnits = new List<ProductionUnitFs>();
-    private List<FrancescoEnergyData> _energyDataEntries = new List<FrancescoEnergyData>();
-    private List<ResultHolder> _resultEntries = new List<ResultHolder>();
+    private List<DataPoint>? _dataPoints = new List<DataPoint>();
+    private List<ProductionUnit> _productionUnits = new List<ProductionUnit>();
+    public List<ResultHolder>? Results { get; private set; }
 
-    public void OptimizerScenario1()
+    public void LoadData()
     {
+        GetDataPoints();
         GetProductionUnits();
-        GetTimeSeriesData();
 
-        // Sorts the production Units by their production cost.
-        _pUnits = _pUnits.OrderBy(o => o.ProductionCost).ToList();
-
-        ComputeAndStoreBoilerCounts();
+        _productionUnits = _productionUnits.OrderBy(o => o.ProductionCost).ToList();
     }
 
-    private bool ComputeAndStoreBoilerCounts()
+    public void OptimizeScenario1()
     {
-        int numberOfBoilers = 0;
-
-        foreach (var entry in _energyDataEntries)
-        {
-            numberOfBoilers = CalculateHeatUnitsRequired(entry);
-            _resultEntries.Add(new ResultHolder(entry.StartTime, entry.EndTime, numberOfBoilers));
-        }
-
-        return true;
+        CalculateHeatUnitsRequired();
     }
 
-    private int CalculateHeatUnitsRequired(FrancescoEnergyData entry)
+    private List<ProductionUnit> SetOperationPoint(List<ProductionUnit> productionUnits, double heatDemand)
     {
-        double totProductionCapacity = 0;
-        int i = 0;
+        List<ProductionUnit> workingUnits = new();
 
-        totProductionCapacity += _pUnits[i].MaxHeat;
-
-        while (entry.HeatDemandMwh > totProductionCapacity)
+        foreach (var unit in productionUnits)
         {
-            totProductionCapacity += _pUnits[i].MaxHeat;
-            i++;
 
-            if (i > _pUnits.Count)
+            // For some reason if I don't clone the object C# confuses to which object I am referring to
+            // and messes up all the objects in the "results" list
+            ProductionUnit unitClone = (ProductionUnit)unit.Clone();
+
+            if (unitClone.MaxHeat > heatDemand)
             {
-                Console.WriteLine("WARNING: HEAT DEMAND CAN NOT BE SATISFIED");
-                throw new Exception("WARNING: HEAT DEMAND CAN NOT BE SATISFIED");
+                double operationPoint = CalculateOperationPoint(heatDemand, unit.MaxHeat);
+
+
+                unitClone.OperationPoint = operationPoint;
+
+                workingUnits.Add(unitClone);
+                return workingUnits;
             }
+
+            unitClone.OperationPoint = 1;
+            heatDemand = heatDemand - unitClone.MaxHeat;
+            workingUnits.Add(unitClone);
         }
 
-        return i;
+        return workingUnits;
+
+        double CalculateOperationPoint(double demand, double productionCapacity)
+        {
+            return Math.Round((demand / productionCapacity), 4);
+        }
     }
 
-    // Method which calculates Hourly NetProductionCost for heat only boilers
+    private void CalculateHeatUnitsRequired()
+    {
+        if (_dataPoints == null)
+        {
+            return;
+        }
+
+        List<ResultHolder> results = new List<ResultHolder>();
+
+        foreach (DataPoint dataPoint in _dataPoints)
+        {
+            // Calculates how many boilers are needed to satisfy heat demand
+            // Implies that they are ordered by efficiency
+            int nOfBoilers = SatisfyHeatDemand(dataPoint);
+
+            //Console.WriteLine("Number of boilers: {0}", nOfBoilers);
+
+            // Selects the correct boilers based on how many need to be activated
+            // Again implies that they are ordered by efficiency
+            List<ProductionUnit> selectedBoilers = SelectBoiler(nOfBoilers);
+
+            //Console.WriteLine("Boilers selected:");
+            //selectedBoilers.ForEach(Console.WriteLine);
+
+            // Sets the operation point of the selected boilers
+            selectedBoilers = SetOperationPoint(selectedBoilers, dataPoint.HeatDemand);
+
+            // Creates an object which holds the result
+            ResultHolder result = new ResultHolder(dataPoint.StartTime, dataPoint.EndTime, dataPoint.HeatDemand,
+                dataPoint.ElectricityPrice, 0, selectedBoilers);
+            // Adds the object to the list
+            results.Add(result);
+            Console.WriteLine(results[^1]);
+            Console.WriteLine(result);
+        }
+
+        Results = results;
+        results.ForEach(Console.WriteLine);
+
+
+        int SatisfyHeatDemand(DataPoint dataPoint)
+        {
+            double currentProductionCapacity = 0;
+            int i = 0;
+
+            while (dataPoint.HeatDemand > currentProductionCapacity)
+            {
+                currentProductionCapacity = currentProductionCapacity + _productionUnits[i].MaxHeat;
+                i++;
+
+                if (i > _productionUnits.Count)
+                {
+                    Console.WriteLine("WARNING: HEAT DEMAND CAN NOT BE SATISFIED");
+                    throw new Exception("WARNING: HEAT DEMAND CAN NOT BE SATISFIED");
+                }
+            }
+            return i;
+        }
+
+        List<ProductionUnit> SelectBoiler(int j)
+        {
+            List<ProductionUnit> selectedBoilers = new List<ProductionUnit>();
+
+            for (int i = 0; i < j; i++)
+            {
+                selectedBoilers.Add(_productionUnits[i]);
+            }
+
+            return selectedBoilers;
+        }
+    }
     public void CalculateNetProductionCost()
     {
-        double hourlyProductionCost = 0;
-
-        foreach (var entry in _resultEntries)
+        // Should I make a copy of the list, set the Production Cost and update the public one OR
+        // keep manipulating it directly like I am now
+        if (Results == null)
         {
-            hourlyProductionCost = GetTotalCost(entry.NumberOfBoilers);
-            Console.WriteLine(hourlyProductionCost + "\tkr");
+            return;
         }
 
-        double GetTotalCost(int numberOfBoilers)
+        int i = 0;
+
+        foreach (var entry in Results)
         {
-            double totalCost = 0;
+            double hourlyProductionCost = Results[i].Boilers.Sum(o => o.ProductionCost);
 
-            for (int i = 0; i <= numberOfBoilers; i++)
-            {
-                totalCost += _pUnits[i].ProductionCost;
-            }
+            Results[i].NetProductionCost = hourlyProductionCost;
 
-            return totalCost;
+            i++;
         }
     }
 
-    // Tiny class used to hold results calculated by the Scenario 1 optimizer.
-    // Needs to be refactored deleted once RDM is implemented
-    private class ResultHolder(DateTime startTime, DateTime endTime, int numberOfBoilers)
+    public void LogResults()
     {
-        public DateTime StartTime { get; set; } = startTime;
-        public DateTime EndTime { get; set; } = endTime;
-        public int NumberOfBoilers { get; set; } = numberOfBoilers;
-
-        public override string ToString()
+        if (Results == null)
         {
-            string s = string.Concat(StartTime, " ", EndTime, " ", NumberOfBoilers);
-            return s;
+            return;
+        }
+        Results.ForEach(Console.WriteLine);
+    }
+
+    private void GetDataPoints()
+    {
+        IDataSource dataSource = new CsvDataSource();
+
+        SourceDataManager.SourceDataManager sourceDataManager = new(dataSource, "../../../../Assets/winter_period.csv");
+
+        Task fetchTimeSeriesDataAsync = sourceDataManager.FetchTimeSeriesDataAsync();
+
+        fetchTimeSeriesDataAsync.Wait();
+
+        //sourceDataManager.LogTimeSeriesData();
+
+        _dataPoints = sourceDataManager.TimeSeriesData;
+    }
+
+    public void LogDataPoints()
+    {
+        if (_dataPoints == null)
+        {
+            return;
+        }
+
+        foreach (DataPoint dataPoint in _dataPoints)
+        {
+            Console.WriteLine(dataPoint);
         }
     }
 
-    // Will eventually call AssetManager to get the production units.
-    // Right now it just initialises two and adds them to the list.
-    // SOLID is applied: if I add productionUnits by calling the assetManager it will behave in the same way.
-    private bool GetProductionUnits()
+    // Will call Asset Manager eventually.
+    private void GetProductionUnits()
     {
-        ProductionUnitFs oilBoiler = new ProductionUnitFs("Oil boiler", 0, 4, 700, 0, 1.2, 265);
-        ProductionUnitFs gasBoiler = new ProductionUnitFs("Gas boiler", 0, 5, 500, 0, 1.1, 215);
-        ProductionUnitFs fantasyBoiler = new ProductionUnitFs("Fantasy boiler", 0, 3, 1000, 0, 1.4, 300);
-        _pUnits.Add(oilBoiler);
-        _pUnits.Add(fantasyBoiler);
-        _pUnits.Add(gasBoiler);
+        ProductionUnit controlBoiler = new ProductionUnit("Control Boiler", "", 5,800,0,1.5,310);
+        ProductionUnit gasBoiler = new ProductionUnit("Gas Boiler", "", 5,500,0,1.1,215);
+        ProductionUnit oilBoiler = new ProductionUnit("Oil Boiler", "", 4,700,0,1.2,265);
 
-        return true;
+        _productionUnits.Add(controlBoiler);
+        _productionUnits.Add(oilBoiler);
+        _productionUnits.Add(gasBoiler);
     }
 
-    // Will eventually call SDM to get the time series.
-    // Right now it just uses the tiny csv controller to load all data into a list
-    private bool GetTimeSeriesData()
+    public void LogProductionUnits()
     {
-        string path = "../Assets/winter_period.csv";
-        FrancescoCsvController francescoCsvController = new FrancescoCsvController(path);
-
-        _energyDataEntries = francescoCsvController.ReadTimeSeriesEnergyData();
-        Console.WriteLine("Loaded entries");
-
-        return true;
+        int i = 0;
+        foreach (ProductionUnit productionUnit in _productionUnits)
+        {
+            Console.WriteLine($"{i} " + productionUnit);
+            i++;
+        }
     }
 }
