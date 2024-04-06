ec63a02565297ab7f447f06d848546b8586dd4a0
Rikki Snaebjornsson <R.Snaebjornsson@gmail.com>
2024-04-06 22:07:25 +0200

    SP2-14 - Added FetchOptimizationData() method.

--- Heatington/ResultDataManager/ResultDataManager.cs
+++ Heatington/ResultDataManager/ResultDataManager.cs
@@ -1,4 +1,6 @@
 using Heatington.Controllers;
+using Heatington.Models;
+using Heatington.Optimizer;
 
 namespace Heatington.ResultDataManager
 {
@@ -7,10 +9,17 @@ namespace Heatington.ResultDataManager
     {
         private readonly string _filePath;
         private readonly List<string> _data = new List<string>();
+        private List<ResultHolder> _optResults;
 
         public ResultDataManager(string filePath)
         {
             _filePath = filePath;
+
+        }
+
+        public void FetchOptimizationData(Opt opt)
+        {
+            _optResults = opt.Results;
         }
 
         public void WriteDataToCsv()
