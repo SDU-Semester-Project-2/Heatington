6dd029547e8fda5a1d3d4a8301db099addbd996f
magwrap <jasiek.musiol@gmail.com>
2024-04-07 20:55:12 +0200

    SP2-86 - reads production units as Dictionary

--- Heatington/AssetManager/AssetManager.cs
+++ Heatington/AssetManager/AssetManager.cs
@@ -4,10 +4,19 @@ using Heatington.Helpers;
 
 namespace Heatington.AssetManager;
 
+/// <summary>
+///
+/// </summary>
+/// <example>
+/// <code>
+/// AssetManager AM = new AssetManager();
+/// await AM.LoadAssets();
+/// </code>
+/// </example>
 public class AssetManager
 {
     public HeatingGrid? HeatingGridInformation;
-    public Dictionary<string, ProductionUnit>? ProductionUnits;
+    public Dictionary<ProductionUnitsEnum, ProductionUnit>? ProductionUnits;
 
     private readonly string _pathToHeatingGrid =
         Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/HeatingGrid.json");
@@ -33,27 +42,46 @@ public class AssetManager
 
     public async Task LoadAssets()
     {
-        Console.WriteLine("Loading assets");
+        Console.WriteLine("Loading assets...");
+
         HeatingGridInformation = await _heatingGridJsonController.ReadData<HeatingGrid>();
-        Console.WriteLine(HeatingGridInformation.Name);
 
-        ProductionUnit[] units;
-        units = await _productionUnitsJsonController.ReadData<ProductionUnit[]>();
+        //TODO: Discuss if ProductionUnits should be stored separately in different files or all in one
+        Dictionary<string, ProductionUnit> prodUnits =
+            await _productionUnitsJsonController.ReadData<Dictionary<string, ProductionUnit>>();
 
+        ProductionUnits = new();
 
-        foreach (var unit in units)
+        // In fetched dictionary string keys are converted into enum keys for code intellisense and readability
+        foreach ((KeyValuePair<string, ProductionUnit> prodUnit, int i) in prodUnits.Select((value, i) => (value, i)))
         {
-            Console.WriteLine(unit);
+            ProductionUnits.Add((ProductionUnitsEnum)i, prodUnit.Value);
         }
     }
 
-    public ProductionUnit ReadHeatingUnits()
+    public Dictionary<ProductionUnitsEnum, ProductionUnit> ReadHeatingUnits()
     {
-        throw new NotImplementedException();
+        if (ProductionUnits == null)
+        {
+            Utilities.DisplayException("ProductionUnits are empty!\nDid you forget to LoadAssets before?");
+            throw new MissingMemberException();
+        }
+
+        // TODO: right now it does nothing but may be useful in different scenarios?
+        Dictionary<ProductionUnitsEnum, ProductionUnit> heatingUnits =
+            ProductionUnits.Where(
+                kvp => kvp.Value.MaxHeat != 0
+            )
+            .ToDictionary(item =>
+                    item.Key, item => item.Value
+            );
+
+        return heatingUnits;
     }
 
-    public void WriteHeatingUnit()
+    public void WriteHeatingUnit(Guid UnitId)
     {
+        // TODO: I'm sure in which way we'll be writing the Heating Units
         throw new NotImplementedException();
     }
 }
