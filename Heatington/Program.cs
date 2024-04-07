997da0f184c32bd8a3c6fdc40f4c1e65e234373f
magwrap <jasiek.musiol@gmail.com>
2024-04-07 02:01:08 +0200

    SP2-82 working example of JsonController

--- Heatington/Program.cs
+++ Heatington/Program.cs
@@ -8,19 +8,3 @@ namespace Heatington
         }
     }
 }
-
-//     // SDM Test
-//     const string filePath = "../Assets/winter_period.csv";
-//
-//     // Create a new CsvDataSource
-//     IDataSource dataSource = new CsvDataSource();
-//
-//     SourceDataManager sdm = new(dataSource, filePath);
-//
-//     // Fetch data from dataSource
-//     sdm.FetchTimeSeriesData();
-//
-//     // Log the loaded data to the console
-//     sdm.LogTimeSeriesData();
-//
-//     Console.WriteLine("Data loaded successfully.");
