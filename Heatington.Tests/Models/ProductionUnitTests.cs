82e594d5b9f39a2a96d653aefc3139ec33af3f98
KonyakB <bencekanyok@gmail.com>
2024-04-12 21:34:54 +0200

    SP2-116 unit test for ProductionUnit

---
+++ Heatington.Tests/Models/ProductionUnitTests.cs
@@ -0,0 +1,40 @@
+using Heatington.Models;
+
+namespace Heatington.Tests.Models
+{
+    public class ProductionUnitTest
+    {
+        [Fact]
+        public void Constructor_Works_Properly()
+        {
+            ProductionUnit unit = new ProductionUnit("Example Unit", "example/path", 20, 25, 30, 35, 40);
+
+            Assert.Equal("Example Unit", unit.Name);
+            Assert.Equal("example/path", unit.PicturePath);
+            Assert.Equal(20, unit.MaxHeat);
+            Assert.Equal(25, unit.ProductionCost);
+            Assert.Equal(30, unit.MaxElectricity);
+            Assert.Equal(35, unit.GasConsumption);
+            Assert.Equal(40, unit.Co2Emission);
+            Assert.Equal(0, unit.OperationPoint);
+        }
+
+        [Fact]
+        public void OperationPoint_ValidRange()
+        {
+            ProductionUnit unit = new ProductionUnit("Example Unit", "example/path", 20, 25, 30, 35, 40);
+
+            unit.OperationPoint = 0.5;
+            Assert.Equal(0.5, unit.OperationPoint);
+        }
+
+        [Fact]
+        public void OperationPoint_OutOfRange()
+        {
+            ProductionUnit unit = new ProductionUnit("Example Unit", "example/path", 20, 25, 30, 35, 40);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => unit.OperationPoint = 1.1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => unit.OperationPoint = -0.1);
+        }
+    }
+}
