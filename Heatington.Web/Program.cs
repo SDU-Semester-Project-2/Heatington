cb3b26d690a8e09771225a988f84dcd008bd0976
magwrap <jasiek.musiol@gmail.com>
2024-04-13 00:34:03 +0200

    SP2-132 added webassembly client support, created basic page structure, set basic theme and layout

--- Heatington.Web/Program.cs
+++ Heatington.Web/Program.cs
@@ -1,3 +1,4 @@
+using Heatington.Web.Client.Pages;
 using Heatington.Web.Components;
 using MudBlazor.Services;
 
@@ -5,14 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddMudServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -25,6 +30,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(DataVisualizer).Assembly);
 
 app.Run();
