5bdfc978a6f38c62a1a921d4e32286f885e90867
frske <144455292+frske@users.noreply.github.com>
2024-04-14 15:55:39 +0200

    SP-125 Program.cs to run the console app

--- Heatington.Console/Program.cs
+++ Heatington.Console/Program.cs
@@ -1,3 +1,9 @@
+using Heatington.AssetManager;
+using Heatington.Controllers;
+using Heatington.Controllers.Interfaces;
+using Heatington.Helpers;
+using Heatington.Optimizer;
+
 namespace Heatington.Console
 {
     internal static class Program
@@ -6,8 +12,22 @@ namespace Heatington.Console
         static async Task Main(string[] args) //DONT REMOVE asnyc Task
         {
             //DONT REMOVE IT'S IMPORTANT FOR THE DOCUMENTATION SERVER
-            await RunDocFx();
+            //await RunDocFx();
+
+            AssetManager.AssetManager am = new AssetManager.AssetManager();
+
+            string fileName = "winter_period.csv";
+            string filePath = Utilities.GeneratePathToFileInAssetsDirectory(fileName);
+            IDataSource dataSource = new CsvController(filePath);
+            SourceDataManager.SourceDataManager srm = new(dataSource);
+
+            Opt opt = new Opt();
+
+            ConsoleUI consoleUi = new ConsoleUI(am, srm, opt);
+
+            consoleUi.StartUi();
         }
+
         private static async Task RunDocFx()
         {
             //updates the documentation on dotnet run
