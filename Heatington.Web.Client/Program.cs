b42be90f70212d8ad90ef3ffc18dedb9a8efa6a5
magwrap <jasiek.musiol@gmail.com>
2024-04-16 00:21:56 +0200

    SP2-128 - moved index.html to client

--- Heatington.Web.Client/Program.cs
+++ Heatington.Web.Client/Program.cs
@@ -1,9 +1,18 @@
+using Heatington.Web.Client;
+using Heatington.Web.Client.Pages;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
+
+
+builder.RootComponents.Add<Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
 
 await builder.Build().RunAsync();
