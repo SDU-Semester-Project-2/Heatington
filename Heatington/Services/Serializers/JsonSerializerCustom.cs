e7c3e01a6aeb9cd0bead4654b40e33b8355b000f
magwrap <jasiek.musiol@gmail.com>
2024-04-21 16:07:18 +0200

    SP2-109 - moved json serializer to a different file for easier testing and file structure

---
+++ Heatington/Services/Serializers/JsonSerializerCustom.cs
@@ -0,0 +1,57 @@
+using System.Text.Json;
+using Heatington.Helpers;
+using Heatington.Services.Interfaces;
+
+namespace Heatington.Services.Serializers;
+
+public class JsonSerializerCustom : ISerializeDeserialize
+{
+    public static string Serialize<T>(T obj)
+    {
+        var serializeOptions = new JsonSerializerOptions { WriteIndented = true };
+
+        // TODO: use async serialzier?
+        // Then you have to pass the StreamWriter to the jsonSerializer
+        // serialzeOptions.Converters.Add(new ProductionJsonConverter());
+
+        try
+        {
+            // byte[] jsonUtf8Bytes =JsonSerializer.SerializeToUtf8Bytes(obj); // --> it's 10% faster
+            // string sdd = JsonSerializer.SerializeAsync(obj, serializeOptions);
+            return JsonSerializer.Serialize(obj, serializeOptions);
+        }
+        catch (Exception e)
+        {
+            Utilities.DisplayException($"Unknown exception occured: {e.Message}");
+            throw;
+        }
+    }
+
+    public static T Deserialize<T>(string file)
+    {
+        var deserializeOptions = new JsonSerializerOptions();
+
+        // TODO: use async deserialzier?
+        // deserializeOptions.Converters.Add(new ProductionUnitJsonConverter());
+
+        try
+        {
+            // JsonSerializer.DeserializeAsync<T>(file, deserializeOptions);
+            T? result = JsonSerializer.Deserialize<T>(file, deserializeOptions);
+
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                throw new JsonException("Couldn't deserialize the object");
+            }
+        }
+        catch (Exception e)
+        {
+            Utilities.DisplayException($"Unknown exception occured: {e.Message}");
+            throw;
+        }
+    }
+}
