f91449f6aa670a3cbea3817f40c8ba909b912bc1
0rangeBlaze <gardevdaniel@gmail.com>
2024-04-23 11:16:18 +0200

    SP2-45 - cleanup and rename

--- Heatington/Controllers/EnerginetApiController.cs
+++ Heatington/Controllers/EnerginetApiController.cs
@@ -15,15 +15,8 @@ namespace Heatington.Controllers
                 new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        /*public static async Task DoSomething(){
-
-            await ProcessRepositoriesAsync(client);
-
-        }*/
-
         public async Task<string> ProcessRepositoriesAsync(string dataset, DateTime start, DateTime end, Dictionary<string, string[]>? filters = null, string[]? columns = null)
         {
-            //Console.WriteLine("{" + String.Join(",", filters.Select(pair => pair.Key + ":[\"" + String.Join("\",\"", pair.Value)  + "\"]" )) + "}");
             filters = filters ?? new();
             columns = columns ?? new string[] {};
             string startString = "start=" + start.ToString("yyyy-MM-ddTHH:mm");
@@ -31,22 +24,12 @@ namespace Heatington.Controllers
             string filterString = "filter={" +
                     String.Join(",", filters.Select(pair => '"' + pair.Key+ '"' + ":[\"" + String.Join("\",\"", pair.Value)  + "\"]" )) +
                 "}";
-            string columnsString = "columns=" + String.Join(",", columns);//HourDK,PriceArea,SpotPriceDKK";
+            string columnsString = "columns=" + String.Join(",", columns);
 
             string rawJson = await _client.GetStringAsync(
                 $"https://api.energidataservice.dk/dataset/" + dataset + '?' + startString+ '&' + endString + '&' + filterString +'&' + columnsString);
-                /*"filter={\"PriceArea\":[\""+String.Join("\",\"", priceAreas)+"\"]}*/
             Console.WriteLine($"https://api.energidataservice.dk/dataset/" + dataset + '?' + startString+ '&' + endString + '&' + filterString +'&' + columnsString);
-            //Console.WriteLine(rawJson);
             return rawJson;
-            /*
-            List<double> result;
-            JsonDocument responseJson = JsonDocument.Parse(rawJson);
-            JsonElement records = responseJson.RootElement.GetProperty("records");
-            result = records.EnumerateArray().Select((x) => {return x.GetProperty("SpotPriceDKK").GetDouble();}).ToList();
-            //result.Select((double x) => {Console.WriteLine(x); return x;}).ToList();
-            return result;
-            */
         }
     }
 
