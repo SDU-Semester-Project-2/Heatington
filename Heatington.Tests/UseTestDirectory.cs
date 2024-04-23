513b2299a84909dc10e033bde4b73314f216edc6
magwrap <jasiek.musiol@gmail.com>
2024-04-23 22:31:09 +0200

    SP2-109 - added UseTestDirectory abstract class it's for unit tests that need to create some files, it creates a test directory in the temporary folder on developers device and then removes it

---
+++ Heatington.Tests/UseTestDirectory.cs
@@ -0,0 +1,41 @@
+namespace Heatington.Tests;
+
+public abstract class UseTestDirectory : IDisposable
+{
+    private const string TestsDirectory = "tests";
+    protected readonly string TestsDirPath = Path.Combine(Path.GetTempPath(), TestsDirectory);
+
+    public UseTestDirectory() // NOT A TEST
+    {
+        // create temporary test folder
+        if (!Directory.Exists(TestsDirPath))
+        {
+            Directory.CreateDirectory(TestsDirPath);
+        }
+        else
+        {
+            ClearTestsDirectory();
+        }
+    }
+    private void ClearTestsDirectory() // NOT A TEST
+    {
+        // clear and remove temporary test folder
+        DirectoryInfo di = new DirectoryInfo(TestsDirPath);
+
+        foreach (FileInfo file in di.GetFiles())
+        {
+            file.Delete();
+        }
+
+        foreach (DirectoryInfo dir in di.GetDirectories())
+        {
+            dir.Delete(true);
+        }
+    }
+
+    void IDisposable.Dispose()
+    {
+        //Clear Tests Directory
+        ClearTestsDirectory();
+    }
+}
