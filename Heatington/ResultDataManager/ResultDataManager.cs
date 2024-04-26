a0aaaf30261e7789b30ef0dc313c632575a01987
frske <144455292+frske@users.noreply.github.com>
2024-04-26 15:53:10 +0200

    SP2-148 Method to get production units inside RDM

--- Heatington/ResultDataManager/ResultDataManager.cs
+++ Heatington/ResultDataManager/ResultDataManager.cs
@@ -9,9 +9,25 @@ public class ResultDataManager(CsvController _csvController)
 {
     private List<ResultHolder> _optResults;
 
+    public ResultDataManager(string filePath)
+    {
+        _csvController = new CsvController(filePath);
+    }
+
     public void FetchOptimizationData(Opt opt)
     {
         _optResults = opt.Results;
+    }
+
+    private List<ProductionUnit> GetProductionUnits()
+    {
+        AssetManager.AssetManager assetManager = new();
+
+        Task loadAssets = assetManager.LoadAssets();
+        loadAssets.Wait();
+        List<ProductionUnit> pUnits = assetManager.ProductionUnits!.Values.ToList();
+
+        return pUnits;
     }
 
     public void WriteDataToCsv()
