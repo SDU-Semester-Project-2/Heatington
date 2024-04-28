b2bca13321c9386f891c43ae4020d1e6b487e8c3
phongsakon <pk@yeager.gmbh>
2024-04-28 10:12:17 +0200

    SP2-165-Add TODO comments for future LocalStorage implementation
    Two TODO comments were introduced in the LocalStorageService.cs and ResourceManager.razor files. They indicate the potential use of a SOLID solution for local storage service implementation in the future, while currently focusing on functional application development.

--- Heatington/Services/LocalStorage/LocalStorageService.cs
+++ Heatington/Services/LocalStorage/LocalStorageService.cs
@@ -2,7 +2,8 @@ using Blazored.LocalStorage;
 using Heatington.Models;
 
 namespace Heatington.Services.LocalStorage;
-
+// TODO: May implement this later if everything is working and I have time to refactor. This would be a SOLID solution,
+// but it will increase the layer and I currently mainly focus on functional application.
 public class LocalStorageService(ILocalStorageService localStorage)
 {
     private readonly ILocalStorageService _localStorage = localStorage;
