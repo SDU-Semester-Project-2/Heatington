1e37f0794b94b5f83f0ef5452d7620733fc99474
0rangeBlaze <gardevdaniel@gmail.com>
2024-04-29 21:31:27 +0200

    SP2-102 - tests for CsvController

--- Heatington/Models/DataPoint.cs
+++ Heatington/Models/DataPoint.cs
@@ -4,7 +4,7 @@ using Heatington.Services.Serializers;
 
 namespace Heatington.Models
 {
-    public class DataPoint
+    public class DataPoint : IEquatable<DataPoint>
     {
         [CsvConstructor]
         public DataPoint(string startTime, string endTime, string heatDemand, string electricityPrice)
@@ -84,5 +84,42 @@ namespace Heatington.Models
 
             return s;
         }
+
+        //needed for tests
+        public override bool Equals(object obj) => this.Equals(obj as DataPoint);
+
+        public bool Equals(DataPoint p)
+        {
+            if (p is null)
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(this, p))
+            {
+                return true;
+            }
+            if (this.GetType() != p.GetType())
+            {
+                return false;
+            }
+            return (StartTime == p.StartTime) && (EndTime == p.EndTime) && (HeatDemand == p.HeatDemand) && (ElectricityPrice == p.ElectricityPrice);
+        }
+
+        public override int GetHashCode() => (StartTime, EndTime, HeatDemand, ElectricityPrice).GetHashCode();
+
+        public static bool operator ==(DataPoint lhs, DataPoint rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(DataPoint lhs, DataPoint rhs) => !(lhs == rhs);
     }
 }
