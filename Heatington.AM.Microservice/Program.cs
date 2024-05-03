71a60d55d930dbea90c9993807e205a8f128df73
0rangeBlaze <gardevdaniel@gmail.com>
2024-05-03 11:46:17 +0200

    SP2-157 - Adding controller class

--- Heatington.AM.Microservice/Program.cs
+++ Heatington.AM.Microservice/Program.cs
@@ -1,31 +1,20 @@
-using Heatington.Controllers;
-using Heatington.Controllers.Interfaces;
-using Heatington.Helpers;
-using Heatington.AssetManager;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace AssetManagerAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-// Asset Manager with controllers and paths to assets
-string pathToHeatingGrid =
-    Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/HeatingGrid.json");
-string pathToProductionUnits =
-    Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/ProductionUnits.json");
-
-IReadWriteController heatingGridJsonController = new JsonController(pathToHeatingGrid);
-IReadWriteController productionUnitsJsonController = new JsonController(pathToProductionUnits);
-AssetManager assetManager =
-    new AssetManager(
-        heatingGridJsonController,
-        productionUnitsJsonController
-    );
-
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/productionUnits", () => {
-    Task loadAssets = assetManager.LoadAssets();
-    loadAssets.Wait();
-    return assetManager.ProductionUnits!.Values.ToList();
-});
-
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
