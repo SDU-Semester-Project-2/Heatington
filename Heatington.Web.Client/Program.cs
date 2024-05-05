96199f9658075d0903f8a9d9badcc43760b4d6c5
phongsakon <pk@yeager.gmbh>
2024-05-05 12:01:28 +0200

    SP2-157-Refactor list of boilers into separate class file
    Moved list of boilers along with its related methods and classes into a separate 'ResourceManager.razor.cs' class file from 'ResourceManager.razor'. Removed the unused 'MudServices' from 'Program.cs'. These changes make the code clearer and better structured. Will try to implement the api now to frontend

--- Heatington.Web.Client/Program.cs
+++ Heatington.Web.Client/Program.cs
@@ -12,7 +12,6 @@ builder.RootComponents.Add<Routes>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
 
 
 await builder.Build().RunAsync();
