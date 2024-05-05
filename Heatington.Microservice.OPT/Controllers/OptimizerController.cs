c12488a088328aa425483fb8f575208910226942
magwrap <jasiek.musiol@gmail.com>
2024-05-05 19:05:55 +0200

    SP2-160-basic opt api functionality

--- Heatington.Microservice.OPT/Controllers/OptimizerController.cs
+++ Heatington.Microservice.OPT/Controllers/OptimizerController.cs
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
 using Heatington.Microservice.OPT.Services;
-=======
->>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
 using Heatington.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Heatington.Microservice.OPT.Controllers
 {
-<<<<<<< HEAD
     // FOR TESTING
     // http://localhost:5019/swagger/index.html
 
-=======
->>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
     [Route("api/[controller]")]
     [ApiController]
     public class OptimizerController : ControllerBase
     {
-<<<<<<< HEAD
         // http://localhost:5019/api/optimizer?orderBy=a&orderBy=b&orderBy=c
         // passes: string[] orderby = ["a", "b", "c"]
 
@@ -41,21 +34,6 @@ namespace Heatington.Microservice.OPT.Controllers
             {
                 return StatusCode(500, e.Message);
             }
-=======
-        // pass string[] orderby = ["a", "b", "c"]
-        // http://localhost:5019/api/optimizer?orderBy=a&orderBy=b&orderBy=c
-
-        [HttpGet]
-        public ActionResult<List<ResultHolder>?> Get([FromQuery] string[] orderBy)
-        {
-            // foreach (string se in orderBy)
-            // {
-            //     Console.WriteLine(se);
-            // }
-
-            OptimizerModel.OPT.Optimize();
-            return Ok(OptimizerModel.OPT.Results);
->>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
         }
     }
 }
--- Heatington.Microservice.OPT/OptimizerModel.cs
+++ Heatington.Microservice.OPT/OptimizerModel.cs
@@ -27,38 +27,86 @@ using Heatington.Services.Interfaces;
 
 namespace Heatington.Microservice.OPT;
 
-// TODO: For calling api inside of api
-// https://stackoverflow.com/questions/69124747/how-to-call-an-api-inside-another-api-on-asp-net-core-2-1
 public class OptimizerModel
 {
-    public static Opt OPT;
+    public static Opt? OPT;
 
     static OptimizerModel()
     {
-        // Mock asset manager
-        // TODO: Change to API CALl
-        string pathToHeatingGrid =
-            Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/HeatingGrid.json");
-        string pathToProductionUnits =
-            Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/ProductionUnits.json");
+        LoadOptimizer().Wait();
+    }
 
-        IReadWriteController heatingGridJsonController = new JsonController(pathToHeatingGrid);
-        IReadWriteController productionUnitsJsonController = new JsonController(pathToProductionUnits);
-        AssetManager.AssetManager am = new(
-            heatingGridJsonController,
-            productionUnitsJsonController
-        );
+    public static async Task LoadOptimizer()
+    {
+        // Asset Manager
+        AssetManager.AssetManager am = await GetAssetManager();
 
-        // Mock source data manager
-        // TODO: Change to API CALl
-        string fileName = "winter_period.csv";
-        string filePath = Utilities.GeneratePathToFileInAssetsDirectory(fileName);
-        IDataSource dataSource = new CsvController(filePath);
-        SourceDataManager.SourceDataManager sdm = new(dataSource);
+        // SourceDataManager
+        SourceDataManager.SourceDataManager sdm = await GetSourceDataManager();
 
         // Optimizer
         OPT = new Opt(am, sdm);
         OPT.LoadData();
 >>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
+    }
+
+    private static async Task<AssetManager.AssetManager> GetAssetManager()
+    {
+        try
+        {
+            string uri = "http://www.contoso.com/";
+            // string uri = "http://localhost:5012";
+
+            using HttpResponseMessage response = await Program.Client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine(responseBody);
+            // string responseBody = await Program.Client.GetStringAsync(uri);
+
+            // TODO: change to actual asset manager
+            string pathToHeatingGrid =
+                Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/HeatingGrid.json");
+            string pathToProductionUnits =
+                Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/ProductionUnits.json");
+
+            IReadWriteController heatingGridJsonController = new JsonController(pathToHeatingGrid);
+            IReadWriteController productionUnitsJsonController = new JsonController(pathToProductionUnits);
+
+            return new(
+                heatingGridJsonController,
+                productionUnitsJsonController
+            );
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+            throw;
+        }
+    }
+
+    private static async Task<SourceDataManager.SourceDataManager> GetSourceDataManager()
+    {
+        try
+        {
+            string uri = "http://www.contoso.com/";
+            // string uri = "http://localhost:5012";
+            using HttpResponseMessage response = await Program.Client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine(responseBody);
+
+            //TODO: Change to actual sdm
+            string fileName = "winter_period.csv";
+            string filePath = Utilities.GeneratePathToFileInAssetsDirectory(fileName);
+            IDataSource dataSource = new CsvController(filePath);
+            return new(dataSource);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+            throw;
+        }
     }
 }
