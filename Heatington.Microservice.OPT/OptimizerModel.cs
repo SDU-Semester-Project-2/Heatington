f0dc5ea2039003708c6849bf05da355eb78793c5
magwrap <jasiek.musiol@gmail.com>
2024-05-05 19:05:55 +0200

    SP2-160-basic opt api functionality

--- Heatington.Microservice.OPT/OptimizerModel.cs
+++ Heatington.Microservice.OPT/OptimizerModel.cs
@@ -6,8 +6,6 @@ using Heatington.Services.Interfaces;
 
 namespace Heatington.Microservice.OPT;
 
-// TODO: For calling api inside of api
-// https://stackoverflow.com/questions/69124747/how-to-call-an-api-inside-another-api-on-asp-net-core-2-1
 public class OptimizerModel
 {
     public static Opt? OPT;
@@ -28,6 +26,66 @@ public class OptimizerModel
         // Optimizer
         OPT = new Opt(am, sdm);
         OPT.LoadData();
+    }
+
+    private static async Task<AssetManager.AssetManager> GetAssetManager()
+    {
+        try
+        {
+            string uri = "http://www.contoso.com/";
+            // string uri = "http://localhost:5012";
+
+            using HttpResponseMessage response = await Program.Client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine(responseBody);
+            // string responseBody = await Program.Client.GetStringAsync(uri);
+
+            // TODO: change to actual asset manager
+            string pathToHeatingGrid =
+                Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/HeatingGrid.json");
+            string pathToProductionUnits =
+                Utilities.GeneratePathToFileInAssetsDirectory("AssetManager/ProductionUnits.json");
+
+            IReadWriteController heatingGridJsonController = new JsonController(pathToHeatingGrid);
+            IReadWriteController productionUnitsJsonController = new JsonController(pathToProductionUnits);
+
+            return new(
+                heatingGridJsonController,
+                productionUnitsJsonController
+            );
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+            throw;
+        }
+    }
+
+    private static async Task<SourceDataManager.SourceDataManager> GetSourceDataManager()
+    {
+        try
+        {
+            string uri = "http://www.contoso.com/";
+            // string uri = "http://localhost:5012";
+            using HttpResponseMessage response = await Program.Client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine(responseBody);
+
+            //TODO: Change to actual sdm
+            string fileName = "winter_period.csv";
+            string filePath = Utilities.GeneratePathToFileInAssetsDirectory(fileName);
+            IDataSource dataSource = new CsvController(filePath);
+            return new(dataSource);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+            throw;
+        }
     }
 
     private static async Task<AssetManager.AssetManager> GetAssetManager()
