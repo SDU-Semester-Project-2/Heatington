52eabc0d97b0ab2f9890642e442e5d86931932a0
magwrap <jasiek.musiol@gmail.com>
2024-05-05 17:40:36 +0200

    SP2-160-initialized OPT microservice and fixed OPT merge errors

--- Heatington.Microservice.OPT/Controllers/OptimizerController.cs
+++ Heatington.Microservice.OPT/Controllers/OptimizerController.cs
@@ -1,16 +1,23 @@
+<<<<<<< HEAD
 using Heatington.Microservice.OPT.Services;
+=======
+>>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
 using Heatington.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Heatington.Microservice.OPT.Controllers
 {
+<<<<<<< HEAD
     // FOR TESTING
     // http://localhost:5019/swagger/index.html
 
+=======
+>>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
     [Route("api/[controller]")]
     [ApiController]
     public class OptimizerController : ControllerBase
     {
+<<<<<<< HEAD
         // http://localhost:5019/api/optimizer?orderBy=a&orderBy=b&orderBy=c
         // passes: string[] orderby = ["a", "b", "c"]
 
@@ -34,6 +41,21 @@ namespace Heatington.Microservice.OPT.Controllers
             {
                 return StatusCode(500, e.Message);
             }
+=======
+        // pass string[] orderby = ["a", "b", "c"]
+        // http://localhost:5019/api/optimizer?orderBy=a&orderBy=b&orderBy=c
+
+        [HttpGet]
+        public ActionResult<List<ResultHolder>?> Get([FromQuery] string[] orderBy)
+        {
+            // foreach (string se in orderBy)
+            // {
+            //     Console.WriteLine(se);
+            // }
+
+            OptimizerModel.OPT.Optimize();
+            return Ok(OptimizerModel.OPT.Results);
+>>>>>>> c7d5462 (SP2-160-initialized OPT microservice and fixed OPT merge errors)
         }
     }
 }
