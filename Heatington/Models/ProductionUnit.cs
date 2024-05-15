5de500f3964e49012ab758ee9339301bff9f2f49
phongsakon <pk@yeager.gmbh>
2024-05-15 10:34:05 +0200

    SP2-157-Add image fetching and base64 conversion to ResourceManager
    The ResourceManager.razor.cs file has been updated to include fetching images from the API and converting them to base64 format for each production unit. The ProductionUnit model has been expanded with a "PictureBase64Url" property to store the base64 converted images. The display of images on the ResourceManager.razor page has been adjusted to use this new property.

--- Heatington/Models/ProductionUnit.cs
+++ Heatington/Models/ProductionUnit.cs
@@ -2,32 +2,11 @@ namespace Heatington.Models;
 
 public class ProductionUnit : ICloneable
 {
-    public Guid Id { get; }
-    public string Name { get; set; }
-    public string FullName { get; set; }
-    public string PicturePath { get; set; }
     private double _operationPoint;
-    public double MaxHeat { get; set; } // MW
-    public double ProductionCost { get; set; } // DKK/MWh
-    public double MaxElectricity { get; set; } // MW
-    public double GasConsumption { get; set; } // MWh(gas/oil)/MWh
-    public double Co2Emission { get; set; } // kg/MWh
 
-    public double OperationPoint
-    {
-        get { return _operationPoint; }
-        set
-        {
-            if (value < 0 || value > 1)
-            {
-                throw new ArgumentOutOfRangeException("OperationPoint must be between 0 and 1");
-            }
 
-            _operationPoint = value;
-        }
-    }
-
-    public ProductionUnit(string name, string fullName, string picturePath, double maxHeat, double productionCost, double maxElectricity,
+    public ProductionUnit(string name, string fullName, string picturePath, double maxHeat, double productionCost,
+        double maxElectricity,
         double gasConsumption, double co2Emission)
     {
         Id = Guid.NewGuid();
@@ -44,8 +23,34 @@ public class ProductionUnit : ICloneable
 
     // TODO: Take a look at this later, need this now for AddBoilerDialog Component
     public ProductionUnit()
-        : this("", "","", 0.0, 0.0, 0.0, 0.0, 0.0)
+        : this("", "", "", 0.0, 0.0, 0.0, 0.0, 0.0)
     {
+    }
+
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public string FullName { get; set; }
+    public string PicturePath { get; set; }
+    public double MaxHeat { get; set; } // MW
+    public double ProductionCost { get; set; } // DKK/MWh
+    public double MaxElectricity { get; set; } // MW
+    public double GasConsumption { get; set; } // MWh(gas/oil)/MWh
+    public double Co2Emission { get; set; } // kg/MWh
+
+    public string PictureBase64Url { get; set; } // TODO: Check and Update UML
+
+    public double OperationPoint
+    {
+        get { return _operationPoint; }
+        set
+        {
+            if (value < 0 || value > 1)
+            {
+                throw new ArgumentOutOfRangeException("OperationPoint must be between 0 and 1");
+            }
+
+            _operationPoint = value;
+        }
     }
 
 
