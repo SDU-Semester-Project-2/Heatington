d259485181c878fed9a14e433b87436bb8e5f564
KonyakB <bencekanyok@gmail.com>
2024-05-16 19:14:02 +0200

    SP2-159 working SDM API for winter period

--- Heatington.SDM.Microservice/SourceDataManagerModel.cs
+++ Heatington.SDM.Microservice/SourceDataManagerModel.cs
@@ -1,11 +1,8 @@
-namespace Heatington.SourceDataManager
-using Heatington.Controllers;
-
-using Heatington.Controllers;
-using Heatington.Controllers.Interfaces;
+using Heatington.SourceDataManager;
 using Heatington.Services.Interfaces;
 using Heatington.Helpers;
-using Heatington.Optimizer;
+using Heatington.Controllers;
+
 
 
 namespace SourceDataManagerAPI
@@ -18,7 +15,9 @@ namespace SourceDataManagerAPI
             string fileName = "winter_period.csv";
             string filePath = Utilities.GeneratePathToFileInAssetsDirectory(fileName);
             IDataSource dataSource = new CsvController(filePath);
-            SourceDataManager.SourceDataManager SDM = new(dataSource);
+            SDM = new SourceDataManager(dataSource);
+            Task loadTimeSeries = SDM.FetchTimeSeriesDataAsync();
+            loadTimeSeries.Wait();
         }
     }
 }
