1f2fe4597b11b091576e3e97bfeecb36494ec779
magwrap <jasiek.musiol@gmail.com>
2024-05-16 12:30:49 +0200

    SP2-139-changed makefile it should work now please

---
+++ scripts/run_web.sh
@@ -0,0 +1,5 @@
+dotnet run --project ./Heatington.Microservices.AM/Heatington.Microservices.AM.csproj &
+ dotnet run --project ./Heatington.Microservices.SDM/Heatington.Microservices.SDM.csproj &
+ dotnet run --project ./Heatington.Microservice.OPT/Heatington.Microservice.OPT.csproj &
+ dotnet run --project ./Heatington.Microservices.RDM/Heatington.Microservices.RDM.csproj &
+ dotnet run --project ./Heatington.Web/Heatington.Web.csproj
