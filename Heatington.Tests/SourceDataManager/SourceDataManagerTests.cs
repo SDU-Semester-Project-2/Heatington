64b7a1d49e32823b8427277d6fccab5643254a36
phongsakon <pk@yeager.gmbh>
2024-05-16 10:51:29 +0200

    SP2-161-Refactor SourceDataManager unit tests and update test documentation
    The SourceDataManager unit tests have been refactored, replacing CsvController with a custom StubDataSource for improved isolation and control. The FetchTimeSeriesDataAsync test method has been updated to utilize this StubDataSource for data preparation and verification. Associated documentation has also been updated to reflect these changes and provide more detailed explanations of the test methods and their structure.

--- Heatington.Tests/SourceDataManager/SourceDataManagerTests.cs
+++ Heatington.Tests/SourceDataManager/SourceDataManagerTests.cs
@@ -1,36 +1,45 @@
-using Heatington.Controllers;
+using System.Globalization;
 using Heatington.Models;
 using Heatington.Services.Interfaces;
 
-
-// TODO: No test for ConvertApiToCsv yet and LogTimesSeriesData won't be tested because the method will be removed after Implementation of GUI
-
 namespace Heatington.Tests.SourceDataManager
 {
+    public class StubDataSource : IDataSource
+    {
+        public List<DataPoint> Data { get; set; }
+
+        public Task<List<DataPoint>?> GetDataAsync()
+        {
+            return Task.FromResult((List<DataPoint>?)Data);
+        }
+
+        public void SaveData(List<DataPoint> data)
+        {
+            Data = data;
+        }
+    }
+
     public class SourceDataManagerTest
     {
-        private readonly CsvController _csvController;
         private readonly Heatington.SourceDataManager.SourceDataManager _sourceDataManager;
+        private readonly StubDataSource _stubDataSource;
 
         public SourceDataManagerTest()
         {
-            _csvController = new CsvController("test-path");
-            _sourceDataManager = new Heatington.SourceDataManager.SourceDataManager(_csvController);
+            _stubDataSource = new StubDataSource();
+            _sourceDataManager = new Heatington.SourceDataManager.SourceDataManager(_stubDataSource);
         }
 
         [Theory]
         [InlineData("2/8/23 0:00", "2/8/23 1:00", "6.62", "1190.94")]
-
         [InlineData("2/8/23 0:00", "2/8/23 1:00", "6.62", "1190.94",
             "2/8/23 1:00", "2/8/23 2:00", "6.65", "1190.99",
             "2/8/23 2:00", "2/8/23 3:00", "6.69", "1190.97")]
-
         [InlineData("2/8/23 0:00", "2/8/23 1:00", "6.62", "1190.94",
             "2/8/23 1:00", "2/8/23 2:00", "6.65", "1190.99",
             "2/8/23 2:00", "2/8/23 3:00", "6.69", "1190.97",
             "2/8/23 3:00", "2/8/23 4:00", "6.71", "1190.99",
             "2/8/23 4:00", "2/8/23 5:00", "6.73", "1191.01")]
-
         [InlineData("2/8/23 0:00", "2/8/23 1:00", "6.62", "1190.94",
             "2/8/23 1:00", "2/8/23 2:00", "6.65", "1190.99",
             "2/8/23 2:00", "2/8/23 3:00", "6.69", "1190.97",
@@ -38,7 +47,6 @@ namespace Heatington.Tests.SourceDataManager
             "2/8/23 4:00", "2/8/23 5:00", "6.73", "1191.01",
             "2/8/23 5:00", "2/8/23 6:00", "6.75", "1191.03",
             "2/8/23 6:00", "2/8/23 7:00", "6.77", "1191.05")]
-
         [InlineData("2/8/23 0:00", "2/8/23 1:00", "6.62", "1190.94",
             "2/8/23 1:00", "2/8/23 2:00", "6.65", "1190.99",
             "2/8/23 2:00", "2/8/23 3:00", "6.69", "1190.97",
@@ -50,20 +58,24 @@ namespace Heatington.Tests.SourceDataManager
             "2/8/23 8:00", "2/8/23 9:00", "6.81", "1191.09")]
         public async Task FetchTimeSeriesDataAsync_ShouldFetchDataSuccessfully(params string[] data)
         {
-            //TODO: Mark has to fix, I don't have that much time for that sorry
-            // // Arrange
-            // _csvController.Data = new List<DataPoint>();
-            // for (int i = 0; i < data.Length; i += 4)
-            // {
-            //     _csvController.Data.Add(new DataPoint(data[i], data[i + 1], data[i + 2], data[i + 3]));
-            // }
-            //
-             // // Act
-            // await _sourceDataManager.FetchTimeSeriesDataAsync();
-            //
-            // // Assert
-            // Assert.Equal(_csvController.Data.Count, _sourceDataManager.TimeSeriesData?.Count);
+            // Arrange
+            _stubDataSource.Data = [];
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                _stubDataSource.Data.Add(
+                    new DataPoint(
+                        DateTime.ParseExact(data[i], "M/d/yy H:mm", CultureInfo.InvariantCulture),
+                        DateTime.ParseExact(data[i + 1], "M/d/yy H:mm", CultureInfo.InvariantCulture),
+                        double.Parse(data[i + 2]),
+                        double.Parse(data[i + 3])
+                    ));
+            }
+
+            // Act
+            await _sourceDataManager.FetchTimeSeriesDataAsync();
+
+            // Assert
+            Assert.Equal(_stubDataSource.Data.Count, _sourceDataManager.TimeSeriesData?.Count);
         }
     }
-
 }
