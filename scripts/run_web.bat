4479cc28b8975c7bc50ed15f21c587cee88a8959
Rikki Snaebjornsson <R.Snaebjornsson@gmail.com>
2024-05-16 14:46:12 +0200

    SP2-139 - Minor adjustment to run_web.bat

--- scripts/run_web.bat
+++ scripts/run_web.bat
@@ -1,6 +1,6 @@
 @echo off
-start cmd /c dotnet run --project ./Heatington.Microservices.AM/Heatington.Microservices.AM.csproj
-start cmd /c dotnet run --project ./Heatington.Microservices.SDM/Heatington.Microservices.SDM.csproj
-start cmd /c dotnet run --project ./Heatington.Microservice.OPT/Heatington.Microservice.OPT.csproj
-start cmd /c dotnet run --project ./Heatington.Microservices.RDM/Heatington.Microservices.RDM.csproj
-start cmd /c dotnet run --project ./Heatington.Web/Heatington.Web.csproj
+start dotnet run --project ./Heatington.Microservices.AM/Heatington.Microservices.AM.csproj
+start dotnet run --project ./Heatington.Microservices.SDM/Heatington.Microservices.SDM.csproj
+start dotnet run --project ./Heatington.Microservice.OPT/Heatington.Microservice.OPT.csproj
+start dotnet run --project ./Heatington.Microservices.RDM/Heatington.Microservices.RDM.csproj
+start dotnet run --project ./Heatington.Web/Heatington.Web.csproj
