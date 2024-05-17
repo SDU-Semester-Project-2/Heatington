711e0e0b4db9ee6b63a2a52cce59671e8618e97e
KonyakB <bencekanyok@gmail.com>
2024-05-17 12:40:32 +0200

    SP2-159 added summer period, api call can be done for both

--- Heatington.SDM.Microservice/TimeSeriesDataController.cs
+++ Heatington.SDM.Microservice/TimeSeriesDataController.cs
@@ -8,9 +8,20 @@ namespace SourceDataManagerAPI.Controllers
     public class TimeSeriesDataController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<List<DataPoint>> Get()
+        public ActionResult<List<DataPoint>> Get(string season)
         {
-            return SourceDataManagerModel.SDM.TimeSeriesData;
+            if(season.ToLower() == "winter")        //http://localhost:5165/api/TimeSeriesData?season=winter
+            {
+                return SourceDataManagerModel.SDM_Winter.TimeSeriesData;
+            }
+            else if(season.ToLower() == "summer")   //http://localhost:5165/api/TimeSeriesData?season=summer
+            {
+                return SourceDataManagerModel.SDM_Summer.TimeSeriesData;
+            }
+            else
+            {
+                return BadRequest("Invalid season.");
+            }
         }
     }
 }
