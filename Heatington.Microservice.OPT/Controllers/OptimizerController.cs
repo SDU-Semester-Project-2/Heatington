5ca43cc002cbd9a3b82f486f715ac08de8ae0a6e
magwrap <jasiek.musiol@gmail.com>
2024-05-18 20:51:26 +0200

    SP2-160-implemented SDM api

--- Heatington.Microservice.OPT/Controllers/OptimizerController.cs
+++ Heatington.Microservice.OPT/Controllers/OptimizerController.cs
@@ -16,15 +16,26 @@ namespace Heatington.Microservice.OPT.Controllers
         /// <summary>
         /// Get the optimized data
         /// </summary>
-        /// <a href="http://localhost:5019/api/optimizer?orderBy=a">Example usage</a>
+        /// <a href="http://localhost:5019/api/optimizer?season=winter&amp;mode=2">Example usage</a>
+        /// <a href="http://localhost:5019/api/optimizer">Simplest usage with default values</a>
+        /// <param name="mode">modes: 1->Scenario1, 2->Scenario2, 3->co2 </param>
+        /// <param name="season">seasons: winter, summer </param>
         [HttpGet]
-        public async Task<ActionResult<List<ResultHolder>?>> Get([FromQuery] string[] orderBy)
+        public async Task<ActionResult<List<ResultHolder>?>> Get([FromQuery] int mode = 2,
+            [FromQuery] string season = "summer")
         {
+            Console.WriteLine("\n\nMODE:");
+            Console.WriteLine(mode);
+            if (mode is > 3 or < 0)
+            {
+                return StatusCode(400, "Wrong argument value: mode. Value has to be in range: [0,3]");
+            }
+
             // production units
             List<ProductionUnit> productionUnits = await DependenciesService.GetProductionUnits();
 
             // data points
-            List<DataPoint>? dataPoints = await DependenciesService.GetDataPoints();
+            List<DataPoint>? dataPoints = await DependenciesService.GetDataPoints(season);
 
             // Optimizer
             Optimizer.OPT opt = new Optimizer.OPT(productionUnits, dataPoints);
