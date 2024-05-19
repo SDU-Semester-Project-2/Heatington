c0d3ea831f80d24ee065a417824896ebe9d8cd6f
KonyakB <bencekanyok@gmail.com>
2024-05-19 15:45:15 +0200

    SP2-159 SDM API can now work with real data

--- Heatington.Microservice.SDM/SourceDataManagerModel.cs
+++ Heatington.Microservice.SDM/SourceDataManagerModel.cs
@@ -9,6 +9,9 @@ namespace SourceDataManagerAPI
     {
         public static SDM SDM_Winter;
         public static SDM SDM_Summer;
+        public static SDM SDM_WinterReal;
+        public static SDM SDM_SummerReal;
+
         static SourceDataManagerModel()
         {
             string fileNameWinter = "winter_period.csv";
@@ -18,7 +21,7 @@ namespace SourceDataManagerAPI
 
             IDataSource dataSourceWinter = new CsvController(filePathWinter);
             IDataSource dataSourceSummer = new CsvController(filePathSummer);
-
+            
             SDM_Winter = new SDM(dataSourceWinter);
             SDM_Summer = new SDM(dataSourceSummer);
 
@@ -27,6 +30,27 @@ namespace SourceDataManagerAPI
 
             loadTimeSeriesWinter.Wait();
             loadTimeSeriesSummer.Wait();
+
+            
+            IDataSource dataSourceWinterReal = new RealDataController(filePathWinter);
+            IDataSource dataSourceSummerReal = new RealDataController(filePathSummer);
+
+            Task loadRealDataSourceWinter = dataSourceWinterReal.GetDataAsync();
+            Task loadRealDataSourceSummer = dataSourceSummerReal.GetDataAsync();
+
+            loadRealDataSourceWinter.Wait();
+            loadRealDataSourceSummer.Wait();
+            
+            SDM_WinterReal = new SDM(dataSourceWinterReal);
+            SDM_SummerReal = new SDM(dataSourceWinterReal);
+
+            Task loadTimeSeriesWinterReal = SDM_WinterReal.FetchTimeSeriesDataAsync();
+            Task loadTimeSeriesSummerReal = SDM_SummerReal.FetchTimeSeriesDataAsync();
+            
+            loadTimeSeriesWinterReal.Wait();
+            loadTimeSeriesSummerReal.Wait();
+
+            
         }
     }
 }
