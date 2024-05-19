a491787788955e81450382a206372fca8b40b2fd
magwrap <jasiek.musiol@gmail.com>
2024-05-19 13:46:23 +0200

    SP2-139-fixed spelling error

--- scripts/run_web.bat
+++ scripts/run_web.bat
@@ -1,6 +1,6 @@
 @echo off
-start dotnet run --project ./Heatington.Microservices.AM/Heatington.Microservices.AM.csproj
-start dotnet run --project ./Heatington.Microservices.SDM/Heatington.Microservices.SDM.csproj
+start dotnet run --project ./Heatington.Microservice.AM/Heatington.Microservice.AM.csproj
+start dotnet run --project ./Heatington.Microservice.SDM/Heatington.Microservice.SDM.csproj
 start dotnet run --project ./Heatington.Microservice.OPT/Heatington.Microservice.OPT.csproj
-start dotnet run --project ./Heatington.Microservices.RDM/Heatington.Microservices.RDM.csproj
+start dotnet run --project ./Heatington.Microservice.RDM/Heatington.Microservice.RDM.csproj
 start dotnet run --project ./Heatington.Web/Heatington.Web.csproj
--- scripts/run_web_dev.bat
+++ scripts/run_web_dev.bat
@@ -1,6 +1,6 @@
 @echo off
-start dotnet watch --project ./Heatington.Microservices.AM/Heatington.Microservices.AM.csproj
-start dotnet watch --project ./Heatington.Microservices.SDM/Heatington.Microservices.SDM.csproj
+start dotnet watch --project ./Heatington.Microservice.AM/Heatington.Microservice.AM.csproj
+start dotnet watch --project ./Heatington.Microservice.SDM/Heatington.Microservice.SDM.csproj
 start dotnet watch --project ./Heatington.Microservice.OPT/Heatington.Microservice.OPT.csproj
-start dotnet watch --project ./Heatington.Microservices.RDM/Heatington.Microservices.RDM.csproj
+start dotnet watch --project ./Heatington.Microservice.RDM/Heatington.Microservice.RDM.csproj
 start dotnet watch --project ./Heatington.Web/Heatington.Web.csproj
