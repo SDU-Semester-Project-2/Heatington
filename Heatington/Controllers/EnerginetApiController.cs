5af48564a4b65ddb751280248b961ccd5d73127e
KonyakB <bencekanyok@gmail.com>
2024-05-20 13:36:06 +0200

    SP2-159 fixed energinet api filters

--- Heatington/Controllers/EnerginetApiController.cs
+++ Heatington/Controllers/EnerginetApiController.cs
@@ -22,10 +22,10 @@ namespace Heatington.Controllers
             columns = columns ?? new string[] { };
             string startString = "start=" + start.ToString("yyyy-MM-ddTHH:mm");
             string endString = "end=" + end.ToString("yyyy-MM-ddTHH:mm");
-            string filterString = "filter={" +
-                    String.Join(",", filters.Select(pair => '"' + pair.Key + '"' + ":[\"" + String.Join("\",\"", pair.Value) + "\"]")) +
-                "}";
-            string columnsString = "columns=" + String.Join(",", columns);
+            string filterString = "filter=%7B" +
+                    String.Join("%2C", filters.Select(pair => "%22" + pair.Key + "%22" + "%3A%5B%22" + String.Join("%22%2C%22", pair.Value) + "%22%5D")) +
+                "%7D";
+            string columnsString = "columns=" + String.Join("%2C", columns);
 
             string rawJson = await _client.GetStringAsync(
                 $"https://api.energidataservice.dk/dataset/" + dataset + '?' + startString + '&' + endString + '&' + filterString + '&' + columnsString);
