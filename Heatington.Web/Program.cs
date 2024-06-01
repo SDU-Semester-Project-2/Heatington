01b5bfcbada2e9b037a923e505bd2d7dc94cde55
magwrap <jasiek.musiol@gmail.com>
2024-06-01 12:20:06 +0200

    SP2-102-fixed breaking changes

--- Heatington.Web/Program.cs
+++ Heatington.Web/Program.cs
@@ -3,6 +3,7 @@ using Heatington.Web.Components;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -15,16 +16,16 @@ builder.Services.AddMudServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseWebAssemblyDebugging();
+// }
+// else
+// {
+//     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
 
 app.UseHttpsRedirection();
 
